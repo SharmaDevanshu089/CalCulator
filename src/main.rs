9e537a48d68333516c04e82b5c9d0d7e5c1f3d57
emotional-push <sharma.devanshu089@gmail.com>
2025-07-25 18:58:42 +0530

    IDK What i am doing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+#![allow(dead_code)]
+#[allow(non_snake_case)]
+#[allow(unused_mut)]
 use std::io;
+//I like Dead Code for Debugging
 //This is Calculator App Made By Devanshu
 fn main() {
     //main Function will call the start later
     //for debugging
+    greet();
 }
 fn greet(){
     let msg = "Hello There this is CalCulator created.";
     let nameQues = "Please Enter your name";
+    println!("{}/n{}",msg , nameQues);
     let name: String = input();
     println!("Hello {}", name);
     listOptions(name);
@@ -18,6 +24,15 @@ fn listOptions(name: String) {
     // println!("3 => Multiplication");
     // println!("4 => Division")
     let mut choice:i8 = inputChoice();
+}
+fn executeChoice(choice: i8) {
+    match choice {
+        1 => addition(),
+        _ => greet(),
+    }
+}
+fn addition() {
+    println!("Addition")
 }
 //I need to Create a Input funtion to take input
 fn input() -> String{
