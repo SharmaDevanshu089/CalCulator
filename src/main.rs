2ba6ab4cf670b84a3dd0236c07612c6bb27d718c
emotional-push <sharma.devanshu089@gmail.com>
2025-07-25 16:44:04 +0530

    Created the Input System

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
+use std::io;
 //This is Calculator App Made By Devanshu
 fn main() {
     //main Function will call the start later
+    //for debugging
+    let name = input();
+    print!("{}",name)
 }
-fn greet(){
-    let msg = "Hello There this is CalCulator created.";
-    let nameQues = "Please Enter your name"
-}
-fn listOptions() {
+// fn greet(){
+//     let msg = "Hello There this is CalCulator created.";
+//     let nameQues = "Please Enter your name";
+// }
+// fn listOptions() {
     
+// }
+//I need to Create a Input funtion to take input
+fn input() -> String{
+    //creating a empty mutable string
+    let mut inp: String = String::new();
+    //getting string from module
+    io::stdin()
+        .read_line(&mut inp)
+        .expect("A fatal error has occured");
+    return inp;
 }
