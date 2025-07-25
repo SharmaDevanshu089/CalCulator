8ddbe0217463c3d0248336655d540a5e7a288ab7
emotional-push <sharma.devanshu089@gmail.com>
2025-07-25 18:36:47 +0530

    added input listener for choice

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ use std::io;
 fn main() {
     //main Function will call the start later
     //for debugging
-    let name = input();
-    print!("{}",name)
 }
-// fn greet(){
-//     let msg = "Hello There this is CalCulator created.";
-//     let nameQues = "Please Enter your name";
-// }
-// fn listOptions() {
-    
-// }
+fn greet(){
+    let msg = "Hello There this is CalCulator created.";
+    let nameQues = "Please Enter your name";
+    let name: String = input();
+    println!("Hello {}", name);
+    listOptions(name);
+}
+fn listOptions(name: String) {
+    println!("What Operation Would you like to perform:");
+    println!("1 => Addition");
+    // println!("2 => Subtraction");
+    // println!("3 => Multiplication");
+    // println!("4 => Division")
+    let mut choice:i8 = inputChoice();
+}
 //I need to Create a Input funtion to take input
 fn input() -> String{
     //creating a empty mutable string
@@ -22,4 +28,12 @@ fn input() -> String{
         .read_line(&mut inp)
         .expect("A fatal error has occured");
     return inp;
+}
+// A function is needed for input of numbers
+fn inputChoice() -> i8 {
+    let mut choice:i8 = 0;
+    let tempChoice = input();
+    choice = tempChoice.trim().parse()
+    .expect("You didnt Enter a Number , Terminating Program");
+    return choice;
 }
